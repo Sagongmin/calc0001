ca0a5195b0f32b5e7f5fb2d082644be7cb4c8511
Sagongmin <87919031+Sagongmin@users.noreply.github.com>
2022-06-14 21:24:05 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True,
+  name = 'calc0001',
+  version = '0.0.1',
+  description = 'oss-dev my calculator 0001',
+  author = 'sagongmin',
+  author_email = 'sag00794@gmail.com',
+  url = "https://github.com/Sagongmin/calc0001",
+  download_url = "https://github.com/Sagongmin/calc0001.git",
+  install_requires = ['pytest'],
+  long_description = 'oss-dev calculator python module',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+    "Programming Language :: python :: 3",
+    "Operating System :: OS Independent",
+  ],
+  
+
+)
